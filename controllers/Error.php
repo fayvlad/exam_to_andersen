34626c20c4d9468ad4badc225f1957a3fb0a71cb
fayvlad <fayvlad@ukr.net>
2015-04-06 16:58:01 +0300

    error controller

---
+++ controllers/Error.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class Error extends Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->view->msg = 'Страницы не существует!';
+    }
+
+    public function index()
+    {
+        $this->view->render('error/index');
+    }
+
+}
